6535c41fc395d0443bedf299e642c938fb9347cb
Silvia Bon <bon.silvia@gmail.com>
2018-10-17 00:05:16 +0000

    added product tests

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario  "They navigate from the home page to the product detail page by clicking on a product" do
+    visit "/"
+    first('article.product').find('img').click
+    sleep 2
+    save_screenshot
+  end  
+end
